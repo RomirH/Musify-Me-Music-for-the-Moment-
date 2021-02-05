26db3c64f1bfcf0263032b0bc75a5308f3000c56
Jonathan Cao <jonathan.y.cao@gmail.com>
2021-02-05 11:16:20 -0600

    slight changes to speech2text+analyzer

--- speech2text+analyzer.py
+++ speech2text+analyzer.py
@@ -1,7 +1,8 @@
 from analyzer import AnalyzeTone
-# from rt_speech2text import MyRecognizeCallback
+from rt_speech2text import MyRecognizeCallback
 
 if __name__ == "__main__":
     rt = AnalyzeTone("This is a test sentence. This works!")
     rt.analyze()
     print(rt.result())
+    mrc = MyRecognizeCallback()
