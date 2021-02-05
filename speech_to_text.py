a3f9f41fa4d3ca5eacf659dfe806acdef756ae11
alejandrodavidmalavet <alejandrodavidmalavet@gmail.com>
2021-02-05 11:37:43 -0600

    speech to text update

--- speech_to_text.py
+++ speech_to_text.py
@@ -3,70 +3,44 @@ from ibm_watson import SpeechToTextV1
 from ibm_watson.websocket import RecognizeCallback, AudioSource
 from threading import Thread
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from analyzer import AnalyzeTone
 
 try:
     from Queue import Queue, Full
 except ImportError:
     from queue import Queue, Full
 
-class SpeechToText():
+###############################################
+#### Initalize queue to store the recordings ##
+###############################################
+CHUNK = 1024
+# Note: It will discard if the websocket client can't consumme fast enough
+# So, increase the max size as per your choice
+BUF_MAX_SIZE = CHUNK * 10
+# Buffer to store audio
+q = Queue(maxsize=int(round(BUF_MAX_SIZE / CHUNK)))
 
-    def __init__(self,chunk = 1024, api_key = '1gJQDKekWxkirik9EdSYTYRA42vd0UIk2aFONeAD-aYU'):
-        self.CHUNK = chunk
-        self.BUF_MAX_SIZE = self.CHUNK * 10
-        self.q = Queue(maxsize=int(round(self.BUF_MAX_SIZE / self.CHUNK)))
-        self.audio_source = AudioSource(self.q, True, True)
-        self.authenticator = IAMAuthenticator(api_key)
-        self.speech_to_text = SpeechToTextV1(authenticator=self.authenticator)
-        self.FORMAT = pyaudio.paInt16
-        self.CHANNELS = 1
-        self.RATE = 44100
-        self.audio = pyaudio.PyAudio()
-        print("starting stream")
-        self.stream = self.audio.open(
-            format=self.FORMAT,
-            channels=self.CHANNELS,
-            rate=self.RATE,
-            input=True,
-            frames_per_buffer=self.CHUNK,
-            stream_callback=self.pyaudio_callback,
-            start=False
-        )
-        self.stream.start_stream()
-        try:
-            recognize_thread = Thread(target=self.recognize_using_weboscket, args=())
-            recognize_thread.start()
-            while True:
-                pass
-        except KeyboardInterrupt:
-            # stop recording
-            self.stream.stop_stream()
-            self.stream.close()
-            self.audio.terminate()
-            self.audio_source.completed_recording()
+# Create an instance of AudioSource
+audio_source = AudioSource(q, True, True)
 
-    def recognize_using_weboscket(self,*args):
-        mycallback = MyRecognizeCallback()
-        self.speech_to_text.recognize_using_websocket(audio=self.audio_source,
-                                                content_type='audio/l16; rate=44100',
-                                                recognize_callback=mycallback,
-                                                interim_results=True,
-                                                split_transcript_at_phrase_end=True,
-                                                profanity_filter = False)
-    def pyaudio_callback(self, in_data, frame_count, time_info, status):
-        try:
-            self.q.put(in_data)
-        except Full:
-            pass # discard
-        return (None, pyaudio.paContinue)
+###############################################
+#### Prepare Speech to Text Service ########
+###############################################
+
+# initialize speech to text service
+authenticator = IAMAuthenticator('1gJQDKekWxkirik9EdSYTYRA42vd0UIk2aFONeAD-aYU')
+speech_to_text = SpeechToTextV1(authenticator=authenticator)
 
 # define callback for the speech to text service
 class MyRecognizeCallback(RecognizeCallback):
     def __init__(self):
         RecognizeCallback.__init__(self)
+        self.complete_transcript = []
 
     def on_transcription(self, transcript):
-        print(transcript)
+        print(transcript[0]['transcript'])
+        self.complete_transcript.append(transcript[0]['transcript'])
+        rt = AnalyzeTone(transcript[0]['transcript'])
 
     def on_connected(self):
         print('Connection was successful')
@@ -81,11 +55,75 @@ class MyRecognizeCallback(RecognizeCallback):
         print('Service is listening')
 
     def on_hypothesis(self, hypothesis):
-        print(hypothesis)
+        pass
+        # print(hypothesis)
 
     def on_data(self, data):
-        print(data)
+        pass
+        # print(data)
 
     def on_close(self):
         print("Connection closed")
+        print(self.complete_transcript)
+        return self.complete_transcript
 
+
+# this function will initiate the recognize service and pass in the AudioSource
+def recognize_using_weboscket(*args):
+    mycallback = MyRecognizeCallback()
+    speech_to_text.recognize_using_websocket(audio=audio_source,
+                                             content_type='audio/l16; rate=44100',
+                                             recognize_callback=mycallback,
+                                             interim_results=True,
+                                             split_transcript_at_phrase_end=True)
+
+###############################################
+#### Prepare the for recording using Pyaudio ##
+###############################################
+
+# Variables for recording the speech
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+# define callback for pyaudio to store the recording in queue
+def pyaudio_callback(in_data, frame_count, time_info, status):
+    try:
+        q.put(in_data)
+    except Full:
+        pass # discard
+    return (None, pyaudio.paContinue)
+
+# instantiate pyaudio
+audio = pyaudio.PyAudio()
+
+# open stream using callback
+stream = audio.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    frames_per_buffer=CHUNK,
+    stream_callback=pyaudio_callback,
+    start=False
+)
+
+#########################################################################
+#### Start the recording and start service to recognize the stream ######
+#########################################################################
+
+print("Enter CTRL+C to end recording...")
+stream.start_stream()
+
+try:
+    recognize_thread = Thread(target=recognize_using_weboscket, args=())
+    recognize_thread.start()
+
+    while True:
+        pass
+except KeyboardInterrupt:
+    # stop recording
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+    audio_source.completed_recording()
