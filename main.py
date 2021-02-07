6c118642beb1ee2cf954ec7cfdf1929a118892f5
alejandrodavidmalavet <alejandrodavidmalavet@gmail.com>
2021-02-07 14:32:42 -0600

    Integrated Watson NLP

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from tone_analyzer import ToneAnalyzer
 from speech_to_text import SpeechToText
+from nlp import NLP
 from threading import Thread
 from ibm_watson.websocket import RecognizeCallback
 
@@ -8,14 +8,37 @@ class MyRecognizeCallback(RecognizeCallback):
         RecognizeCallback.__init__(self)
 
     def on_transcription(self, transcript):
-        sentiment_thread = Thread(target=analyze_tone, args=(transcript[0]['transcript'],))
-        sentiment_thread.start()
+        # Opens up a new thread to process speech input
+        nlp_thread = Thread(target=getKeywordsAndEmotion, args=(transcript[0]['transcript'],))
+        nlp_thread.start()
 
-def analyze_tone(sentence):
-    print("\nSpeaker : ", sentence, "\nTones : ",tone_analyzer.analyze(sentence))
+    def on_connected(self):
+        print('Connection was successful')
 
-tone_analyzer = ToneAnalyzer(api_key = '3879mC57DARnUoMaGEXT8F0brMiI06nG2TzcBiBjeKR_')
+    def on_error(self, error):
+        print('Error received: {}'.format(error))
 
-speech_to_text = SpeechToText(api_key = '1gJQDKekWxkirik9EdSYTYRA42vd0UIk2aFONeAD-aYU',callback = MyRecognizeCallback())
+    def on_inactivity_timeout(self, error):
+        print('Inactivity timeout: {}'.format(error))
 
+def getKeywordsAndEmotion(text):
+    # Processes, Prints and Returns the keywords and dominating emotions of the speech input
+    keywords = nlp.process(text)
+
+    for kw in keywords:
+
+        print( 
+            "\nKeyword :",kw[0],
+            "\nEmotion :",kw[1]
+        )
+
+    return keywords
+
+# Begin an instance of the NLP Class
+nlp = NLP(api_key = 'I8V3ujhV1XG3VZDxV8sWc3wcD_CNdmdk4RYgJLP9x3Ne')
+
+# Begin an instance of the SpeechToText class
+speech_to_text = SpeechToText(api_key = '1gJQDKekWxkirik9EdSYTYRA42vd0UIk2aFONeAD-aYU', callback = MyRecognizeCallback())
+
+# Start recording and processing speech
 speech_to_text.start()
