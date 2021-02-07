1385d3afd4612d22166964593ee63aa9f3c0cf55
alejandrodavidmalavet <alejandrodavidmalavet@gmail.com>
2021-02-07 14:55:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from nlp import NLP
 from threading import Thread
 from ibm_watson.websocket import RecognizeCallback
 
+
+
 class MyRecognizeCallback(RecognizeCallback):
     def __init__(self):
         RecognizeCallback.__init__(self)
@@ -21,6 +23,8 @@ class MyRecognizeCallback(RecognizeCallback):
     def on_inactivity_timeout(self, error):
         print('Inactivity timeout: {}'.format(error))
 
+
+
 def getKeywordsAndEmotion(text):
     # Processes, Prints and Returns the keywords and dominating emotions of the speech input
     keywords = nlp.process(text)
@@ -34,11 +38,14 @@ def getKeywordsAndEmotion(text):
 
     return keywords
 
-# Begin an instance of the NLP Class
+
+# Begin an instance of the NLP class
 nlp = NLP(api_key = 'I8V3ujhV1XG3VZDxV8sWc3wcD_CNdmdk4RYgJLP9x3Ne')
+
 
 # Begin an instance of the SpeechToText class
 speech_to_text = SpeechToText(api_key = '1gJQDKekWxkirik9EdSYTYRA42vd0UIk2aFONeAD-aYU', callback = MyRecognizeCallback())
+
 
 # Start recording and processing speech
 speech_to_text.start()
