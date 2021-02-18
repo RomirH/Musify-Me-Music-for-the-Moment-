d39a02ae124264ea490a553f382381935928532a
Julianne Cronin <jujucronin@gmail.com>
2021-02-17 22:30:38 -0600

    added SongQueue to search.py
    created a SongQueue class.  Some selenium installation/browser path modification stuff will be needed to use.  Untested with Chrome as of yet.
    Added a test function for SongQueue class to show functionality.
    Additional functionality of class aside from searching:
    --A queue
    --play and pause videos
    --close tabs

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from speech_to_text import SpeechToText
 from nlp import Keywords
-from search import YouTubeAPI
+from search import YouTubeAPI, SongQueue
 from threading import Thread
 from ibm_watson.websocket import RecognizeCallback
-
 
 
 class MyRecognizeCallback(RecognizeCallback):
@@ -43,6 +42,43 @@ def handleSpeechInput(text):
         kwds = keywords.getKeywords(text)
         youtube.search(kwds)
 
+###commented out prep for integration of SongQueue class###
+#class MyRecognizeCallback(RecognizeCallback):
+#    def __init__(self):
+#        RecognizeCallback.__init__(self, squeue = SongQueue(api_key='AIzaSyCjgFgk2bdUS8cr74K9wiopWDdfXhwgt9g'))
+#        self.squeue = squeue
+#
+#    def on_transcription(self, transcript):
+#        # Opens up a new thread to process speech input and search for a video
+#        search_thread = Thread(target=handleSpeechInput, args=(transcript[0]['transcript'],self.squeue))
+#        search_thread.start()
+#
+#    def on_connected(self):
+#        print('Connection was successful')
+#
+#    def on_error(self, error):
+#        print('Error received: {}'.format(error))
+#        speech_to_text.restart()
+#
+#    def on_inactivity_timeout(self, error):
+#        print('Inactivity timeout: {}'.format(error))
+#
+#def handleSpeechInput(text, squeue):
+#    if "restart music listener" in text.lower():
+#        speech_to_text.restart()
+#
+#    elif "start music listener" in text.lower():
+#        speech_to_text.start()
+#
+#    elif "stop music listener" in text.lower():
+#        speech_to_text.stop()
+#        squeue.clear()
+#
+#    else :
+#        # Processes speech into emotions and keywords
+#        kwds = keywords.getKeywords(text)
+#        squeue.add_by_kw(kwds)
+
 
 
 
@@ -53,10 +89,8 @@ keywords = Keywords(api_key = 'I8V3ujhV1XG3VZDxV8sWc3wcD_CNdmdk4RYgJLP9x3Ne')
 # Begin an instance of the SpeechToText class
 speech_to_text = SpeechToText(api_key = '1gJQDKekWxkirik9EdSYTYRA42vd0UIk2aFONeAD-aYU', callback = MyRecognizeCallback())
 
-
 # Begin an instance of the YouTubeAPI class
 youtube = YouTubeAPI(api_key='AIzaSyCjgFgk2bdUS8cr74K9wiopWDdfXhwgt9g')
-
 
 # Start recording and processing speech
 speech_to_text.start()
--- search.py
+++ search.py
@@ -1,6 +1,11 @@
 import urllib
-import webbrowser
-from googleapiclient.discovery import build
+import webbrowser #for webbrowsing
+from selenium import webdriver #for webbrowsing
+from selenium.webdriver.common.keys import Keys #for sending commands to YouTube
+from googleapiclient.discovery import build #for YouTube API
+import time #for testing
+import sys #for printing errors and exit codes
+from threading import Lock #for thread locks
 #pip install selenium and google-api-python-client
 
 class YouTubeAPI():
@@ -41,3 +46,222 @@ class YouTubeAPI():
         #build URL
         url = "https://www.youtube.com/watch?v=" + first_song
         webbrowser.get('chrome').open(url)
+
+class SongQueue():
+    def __init__(self,api_key, driver='chrome'):
+        """Creates a SongQueue.  Args: YouTube-api-key, driver(optional)"""
+        #currently the only supported method of search is Youtube
+        self.req_format = build("youtube", "v3", developerKey=api_key)  
+        self.queue=[]      #listofurls
+        self.tabs = []
+        self.url_base = "https://www.youtube.com/watch?v="
+        self.qlock = Lock() #queue lock
+        self.tablock = Lock()
+
+           
+        if (driver != 'firefox') and (driver !='chrome'): 
+            sys.stderr.write('SongQueue: supported drivers are \'chrome\' and \'firefox\'')
+            sys.exit(-1)  
+        self.driver = driver
+        #if you're having driver issues, see SongQueue.play_first
+
+    def __del__(self):
+        #avoid leaving sockets hanging?
+        #self.req_format.close()
+        sys.stdout.write('deleted SongQueue.\n')
+
+    def show_queue(self):
+        return self.queue
+
+    def add_by_kw(self, keywords: list, raw_num_to_add = 2):
+        """queries YouTube based on kw and adds to list.  Args: ListOfKeywords, #songs-to-add(optional)"""
+        #default length of songs to add is 5
+        #Return: Returns a json list which includes video url, length, id, etc
+
+        try: 
+            num_to_add = abs(int(raw_num_to_add))
+        except:
+            sys.stderr.write('SongQueue.add_by_kw: invalid number of songs to add')
+            return
+
+        if not len(keywords) > 0:
+            sys.stderr.write('SongQueue.add_by_kw: no keywords provided')
+            return
+
+        LENGTH = 10 #placeholder for length
+        #build query list based on keywords
+        query_list = "music"  #search music by inputting music into query
+        list_length = len(keywords)
+        counter = 0
+        for element in keywords:
+            query_list += element
+            counter += 1
+            if(counter < list_length):
+                # + sign required to attach more elements
+                query_list += "+"
+            
+        #build request
+        request = self.req_format.search().list(q=query_list,     #terms to query
+                                                part="snippet",    #returns also description of vid
+                                                type="video",       #searches only videos
+                                                videoCategoryId='10', #searches music videos (?)
+                                                videoDuration = 'medium') #between 4 and 20 mins
+                                                
+                                                #relatedToVideoId = -------
+                                                #returned results will be related to the id input
+                                                #could be used to find new songs
+        response = request.execute() #send request
+        
+        #print(response.items())
+        #take a look at the json response
+
+
+        #for more info about the response
+        #  https://developers.google.com/youtube/v3/docs/search#resource for individual songs
+        #  https://developers.google.com/youtube/v3/docs/search/list for the list returned
+
+        #adds urls of first num_to_add songs to the queue
+        with self.qlock:
+            for i in range(num_to_add):
+                song = response['items'][i]
+                self.queue.append((self.url_base + song['id']['videoId'], LENGTH))
+
+    def play(self):  
+        """plays opened video""" 
+        #sends the 'k' key, used for play and pause in youtube
+        self._send_key('k', tabs='all')
+
+    def pause(self):
+        """pauses opened video"""
+        #sends the 'k' key, used for play and pause in youtube
+        self._send_key('k', tabs='all')
+
+    def play_first(self): 
+        """plays the first song in queue and remove from queue"""   
+        #if no songs have been queued yet, do nothing
+        if not len(self.queue) > 0:
+            sys.stderr.write('SongQueue.play_first: no songs queued\n')
+            return
+
+        #any issues with self.driver, refer to the following
+        # https://selenium-python.readthedocs.io/installation.html
+        if self.driver == 'chrome':
+            browser = webdriver.Chrome()
+        elif self.driver == 'firefox':
+            #webbrowser.register("firefox", None, webbrowser.BackgroundBrowser("C:\Program Files\Mozilla Firefox\\firefox"))
+            browser = webdriver.Firefox(executable_path='C:\WebDriver\\bin\geckodriver')
+           
+        #retreive earliest added url from song queue
+        (first_url, song_len) = self.queue.pop(0) 
+        #length could be useful in determining when to play next, maybe a timer as class attribute
+        sys.stdout.write('playing a song for'+ str(song_len)+'s\n')
+        #it would be cool to save the song title too, so we can give preview of next
+
+        #play next song and add to open tabs
+        browser.get(first_url)
+        with self.tablock:
+            self.tabs.append(browser)
+        #webbrowser.get('firefox').open(url)
+
+    def close_tab(self):
+        if not len(self.tabs) > 0:
+            sys.stderr.write('SongQueue.close_tab: no tabs open to close\n')
+            return
+
+        with self.tablock:    
+            tab = self.tabs.pop(0)
+            tab.close()
+
+    def clear(self):
+        """empties the song queue"""
+        with self.qlock:
+            self.queue = []
+
+    def _send_key(self, key, tabs='one'):
+        """sends a key to the browser opened by the queue.  Args: key(str)"""
+        #sends the 'k' key, used for play and pause in youtube
+
+        with self.tablock:
+            for tab in self.tabs:
+                elem = tab.find_element_by_xpath("/html")
+                try: 
+                    elem.send_keys(key) 
+                    if tabs == 'one':
+                        break 
+                except Exception as e:
+                    sys.stderr.write('send {} key failed\n'.format(key))
+                    print(e)
+                    sys.exit(-1)
+
+def test_SongQueue():
+    KW1 = ['yunosuke', 'blank', 'catalyst']
+    KW2 = ['reol', 'end']
+    KW3 = ['tolerance', 'exotica']
+    KEY = 'AIzaSyCjgFgk2bdUS8cr74K9wiopWDdfXhwgt9g'
+    PAUSE = 5
+    DRIVER = 'firefox'
+
+    try: queue=SongQueue(KEY, driver=DRIVER)
+    except Exception as e: 
+        sys.stderr.write('could not create SongQueue\n')
+        print(e)
+
+    try: queue.add_by_kw(KW1)
+    except Exception as e: 
+        sys.stderr.write('could not query {}\n'.format(KW1))
+        print(e)
+
+    try:queue.play_first()
+    except Exception as e: 
+        sys.stderr.write('SongQueue.play_first failed\n')
+        print(e)
+
+    time.sleep(PAUSE)
+    try: queue.clear()
+    except exception as e:
+        sys.stderr.write('SongQueue.clear failed\n')
+        print(e)
+
+    try:queue.add_by_kw(KW2)
+    except Exception as e: 
+        sys.stderr.write('could not query {}\n'.format(KW2))
+        print(e)
+    
+    try:queue.pause()
+    except Exception as e: 
+        sys.stderr.write('SongQueue.pause failed\n')
+        print(e)
+
+    time.sleep(PAUSE)
+
+    try:queue.play()
+    except Exception as e: 
+        sys.stderr.write('SongQueue.play failed\n')
+        print(e)
+
+    time.sleep(PAUSE)
+
+    try: queue.close_tab()
+    except Exception as e:
+        sys.stderr.write('SongQueue.pause failed\n')
+        print(e)
+
+    try:queue.play_first()
+    except Exception as e: 
+        sys.stderr.write('SongQueue.play_first failed\n')
+        print(e)
+
+    try:queue.add_by_kw(KW3)
+    except Exception as e: 
+        sys.stderr.write('could not query {}\n'.format(KW3))
+        print(e)
+
+    try:
+        sys.stdout.write('current queue: ')
+        print(queue.show_queue())
+    except Exception as e:
+        sys.stderr.write('an error occured while printing queue')
+        print(e)
+    sys.stdout.write('SongQueue tests complete')
+
+test_SongQueue()
